2d57e9a3234046787c8a31e4077f1846922f15db
Riyazh D <riyazh1000@yahoo.com>
2019-03-10 09:21:55 -0400

    multiple file changes
    multiple file changes

--- firebaseApp.js
+++ firebaseApp.js
@@ -1,3 +1,4 @@
+function send() {
 var config = {
     apiKey: "AIzaSyCXvapOvolimSaPdSSTvyPuBED4Xi5xSSM",
     authDomain: "beready-123.firebaseapp.com",
@@ -10,8 +11,9 @@ var config = {
   var firestore = firebase.firestore();
 
   const docRef = firestore.doc("subscriptions/email";)
-
   const textToSave = inputTextFieldValue.value;
+  
+
   docRef.set({
       email: textToSave
   }).then(function() {
@@ -22,9 +24,13 @@ var config = {
 
   const docRef2 = firestore.doc("incidents/records";)
 
-  const incident1 = textIn.value;
-  const incident2 = textAdd.value;
-  const incident3 = file.value;
+  const incident1 = document.querySelector("#input1");
+  const incident2 = document.querySelector("#input2");
+  const incident3 = document.querySelector("#file");
+  const saveButton = document.querySelector("#saveButton");
+
+
+saveButton.addEventListener("click", function() {
   docRef.set({
       in: incident1
       add: incident2
@@ -34,3 +40,5 @@ var config = {
   }).catch(function (error) {
     console.log("Got an error", error);
   });
+})
+}
